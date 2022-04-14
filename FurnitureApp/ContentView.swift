11a8f2436cc1951f38a75ec9542763bd0fe83d47
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-14 12:47:21 +0530

    Initial Commit

---
+++ FurnitureApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FurnitureApp
+//
+//  Created by Bhumika Patel on 14/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Bhumika Patel on 14/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
